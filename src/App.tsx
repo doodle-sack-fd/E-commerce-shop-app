a6bea4c526ba2736db2a0f0b017d0394767e5b8b
Maxim <titov3000@mail.ru>
2023-03-23 12:43:12 +0300

    errorBlock/ footerBlock

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
+import { Route} from 'react-router-dom';
+
 import './App.css';
-import Categories from './components/Categories/Categories';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import MainBlock from './components/MainBlock/MainBlock';
+import './scss/global.scss';
+
 import Home from './pages/Home';
-import './scss/global.scss'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+        <Home />
     </div>
   );
 }
